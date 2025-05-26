0248055abe46c0288bf7636f02c60edf4215aca6
Jared Mercer <jaredthebackpacker@gmail.com>
2025-05-26 09:55:19 -0700

    chore: add scss and global styles

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import "../global.scss";
+  let { children } = $props();
+</script>
+
+<main>
+  {@render children()}
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,15 @@
-<h1>HELLO WORLD</h1>
+<script lang="ts">
+</script>
+
+<style lang="scss">
+  @use "../styles/breakpoints" as *;
+  /* mobile styles*/
+
+  @media (min-width: $tablet) {
+    /* tablet styles */
+  }
+
+  @media (min-width: $desktop) {
+    /* desktop styles */
+  }
+</style>
