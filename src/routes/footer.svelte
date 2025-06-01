a7a58b4537856e870353b17d700f539060ff2b55
Jared Mercer <jaredthebackpacker@gmail.com>
2025-06-01 13:57:55 -0700

    feat: finish main route styles, add hover & focus styles

--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -2,6 +2,10 @@
   <a class="footer-logo" href="/">Triple C Newsletter</a>
   <span class="neutral">C10-0000551-LIC</span>
   <span class="neutral">Copyright © 2025 YCNIUQ Inc.</span>
+  <ul>
+    <li><a href="/privacy-policy">Privacy Policy</a></li>
+    <li><a href="/terms-of-use">Terms of Use</a></li>
+  </ul>
   <span class="attribution">
     Made by
     <a href="https://jaredthecomputerguy.dev/" target="_blank">Jared Mercer</a>
@@ -12,6 +16,7 @@
   @use "../styles/variables" as *;
 
   footer {
+    width: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -29,9 +34,15 @@
     letter-spacing: 0.025em;
     text-transform: uppercase;
     color: #fefefe;
-    transition: all 300ms ease-in-out;
     padding: 0.5rem;
     border-radius: 0.25rem;
+    outline: none;
+    transition: all 150ms ease-in-out;
+
+    &:focus {
+      outline: 2px solid white;
+      outline-offset: 2px;
+    }
   }
 
   .footer-logo:hover {
@@ -43,8 +54,36 @@
     font-size: 0.875rem;
   }
 
+  ul {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    font-size: 0.875rem;
+  }
+
+  ul > li {
+    list-style: none;
+    margin-top: 0.5rem;
+  }
+
+  li > a {
+    padding: 0.25rem 0.5rem;
+    border-radius: 0.25rem;
+    outline: none;
+    transition: all 150ms ease-in-out;
+
+    &:focus {
+      outline: 2px solid white;
+      outline-offset: 2px;
+    }
+
+    &:hover {
+      background-color: rgba(255, 255, 255, 0.1);
+    }
+  }
+
   .attribution {
-    padding: 0.5rem;
+    padding: 0.5rem 0.25rem;
     font-size: 0.875rem;
     display: flex;
     flex-direction: column;
@@ -56,12 +95,26 @@
     font-size: 0.75rem;
     padding: 0.25rem 0.5rem;
     border-radius: 0.25rem;
+    outline: none;
+    transition: all 150ms ease-in-out;
+    &:hover {
+      background-color: rgba(255, 255, 255, 0.1);
+    }
+
+    &:focus {
+      outline: 2px solid white;
+      outline-offset: 2px;
+    }
   }
 
-  .attribution a:hover {
-    background-color: rgba(255, 255, 255, 0.1);
+  @media (min-width: $tablet) {
+    .footer-logo {
+      font-size: 2rem;
+    }
   }
-
-  @media (max-width: $tablet) {
+  @media (min-width: $desktop) {
+    .footer-logo {
+      font-size: 2.5rem;
+    }
   }
 </style>
