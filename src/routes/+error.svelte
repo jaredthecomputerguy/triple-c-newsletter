f601134be9069a017f79a89484f1d046b82537cc
Jared Mercer <jaredthebackpacker@gmail.com>
2025-07-27 15:38:43 -0700

    chore: add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from "$app/state";
+  import Seo from "$lib/seo.svelte";
+  // eslint-disable-next-line
+  console.error("Error: ", page.error?.message);
+</script>
+
+<Seo
+  title="Triple C Newsletter"
+  description="Join the Triple C Collective newsletter and be the first to know about weekend BOGOs, new product drops, and subscriber-only perks."
+  image="/opengraph-image.png"
+  domain="triplecnewsletter.com"
+  path="/" />
+
+<section>
+  <h1>404</h1>
+  <p>Not Found</p>
+</section>
